b0fd3015364e09787918fdc3c0484c4ad7417b6b
robocopkaka <robocopkaka@gmail.com>
2016-08-17 11:42:58 +0100

    Commit includes the create_notes, list_notes, and get methods

---
+++ notes_application.js
@@ -0,0 +1,25 @@
+class NotesApplication{
+	constructor(author){
+		this.author = author;
+		this.notes = [];
+	}
+	
+	create_notes(note_content){
+		this.notes.push(note_content);
+	}// end of create_notes
+	
+	list_notes(){
+		for(var count = 0; count < this.notes.length; count++){
+			console.log("Note ID: " + count);
+			console.log("[" + this.notes[count] + "]");
+			console.log("By Author [" + this.author + "]")
+		}// nd for
+	}// end of list_notes 
+	
+	get(note_id){
+		if(this.notes.hasOwnProperty(note_id)){
+			return this.notes[note_id]
+		}// end if
+		else console.log("ID is invalid");
+	}// end of get
+}
