b7dbd06be111b75139d93ff6dedf2cfa17706f51
robocopkaka <robocopkaka@gmail.com>
2016-08-17 14:15:52 +0100

    Final pre - documentation version

--- notes_application.js
+++ notes_application.js
@@ -1,34 +1,32 @@
 class Notes{
 	constructor(properties){
 		this.author = properties.author;
-		this.notes = properties.note_content;
+		this.note_content = properties.note_content;
 	}
 }
 
 class NotesApplication{
 	constructor(){
-		this.notes = {}; 
+		this.notes = [];
 	}
-	create_notes(new_note){
-		if(!this.notes.hasOwnProperty(new_note.author)){
-			this.notes[new_note.author] = [];
+	
+	create_notes(note){
+		if(note instanceof Notes){
+			this.notes.push(note);
 		}
-		else{
-			this.notes[new_note.author].push(new_note);
-		}
-	}// end of create_notes
+	}
 	
 	list_notes(){
 		for(var count = 0; count < this.notes.length; count++){
 			console.log("Note ID: " + count);
-			console.log("[" + this.notes[count] + "]");
-			console.log("By Author [" + this.author + "]")
+			console.log("[" + this.notes[count].note_content + "]");
+			console.log("By Author [" + this.notes[count].author + "]")
 		}// nd for
 	}// end of list_notes 
 	
 	get(note_id){
 		if(this.notes.hasOwnProperty(note_id)){ //checks to see if the notes id is an index of 
-			return this.notes[note_id]
+			return this.notes[note_id].note_content;
 		}// end if
 		else console.log("ID is invalid");
 	}// end of get
@@ -36,9 +34,9 @@ class NotesApplication{
 	search(search_text){
 		console.log("Showing results for search \‘[<search_text>]\’");
 		for(var count = 0; count< this.notes.length; count++){
-			if(this.notes[count].indexOf(search_text) !== -1){
+			if(this.notes[count].note_content.indexOf(search_text) !== -1){
 				console.log("Note ID: " + count);
-				console.log("[" + this.notes[count] + "]");
+				console.log("[" + this.notes[count].note_content + "]");
 			}// end if
 			
 			else{
@@ -49,12 +47,13 @@ class NotesApplication{
 	}//end of search
 	
 	edit(note_id, new_content){
-		if(this.notes.hasOwnProperty(note_id)) this.notes[note_id] = new_content;
+		if(this.notes.hasOwnProperty(note_id)) this.notes[note_id].note_content = new_content;
 		else console.log("ID is invalid");
 	}// end of edit
 	
 	delete(note_id){
-		if(this.notes.hasOwnProperty(note_id)) this.notes[note_id] = "";
+		if(this.notes.hasOwnProperty(note_id)) this.notes[note_id].note_content = "";
 		else console.log("ID is invalid");
 	}//end of delete
+
 }
